053b2d64147f4d3c65069d4e7d19ca649e4107df
harisssi <haris.khan@ssi-schaefer.ca>
2022-09-20 09:37:35 -0400

    tesl

---
+++ src/main/java/com/example/ssoi/SsoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssoi/SsoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
